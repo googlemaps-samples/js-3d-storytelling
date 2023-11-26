11a5d605a0e2c0d9d9c53b0d6ace009b35640275
immobe <i.beeck@gmail.com>
2023-11-26 16:36:48 +0100

    feat(sidebar): add ability to collpase sidebar

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import { initializeCesiumViewer } from "./utils/cesium.js";
+import { addSidebarToggleHandler } from "./sidebar/sidebar.js";
 
 async function main() {
   try {
     await initializeCesiumViewer();
+
+    addSidebarToggleHandler();
   } catch (error) {
     console.error(error);
   }
