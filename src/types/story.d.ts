3fb2e96ee8f7ac3f76b378bd4ede72f4390510fa
Frank Mecklenburg <mecklenburg@ubilabs.net>
2023-11-29 11:39:37 +0100

    chore(types): update story and chapter types

--- src/types/story.d.ts
+++ src/types/story.d.ts
@@ -7,7 +7,7 @@ type CesiumCameraOptions = {
 };
 
 // Define the type for the Segment
-type Location = {
+interface Chapter = {
   title: string; // Title of the segment
   imageUrl: string; // URL for the segment's image (google or custom)
   imageCredit: string; // Credit for the segment's image
@@ -18,9 +18,16 @@ type Location = {
   cameraOptions: CesiumCameraOptions; // Basic Cesium camera options
 };
 
+interface StoryProperties {
+  imageUrl: string; // The image url for the intro card
+  title: string; // The title of the story
+  date: string; // A date associated with the story
+  description: string; // The description text
+  createdBy: string; // The author
+  startButtonText: string; // The text in the button to start the story
+}
 // Define the NeighbourhoodStory as an array of Segments
 interface Story {
-  segments: Segment[];
-  storyTitle: string;
-  createdBy: string;
+  chapters: Chapter[];
+  properties: StroyProperties;
 }
