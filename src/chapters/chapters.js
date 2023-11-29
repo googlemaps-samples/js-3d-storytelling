d8adaf1d0f2cb6dfba54dd8421dd47bcd398d6f9
Leslie Lawendel <zimmermann@ubilabs.net>
2023-11-29 13:55:05 +0100

    feat(chapters-bar): add navigation buttons to the chapters bar

--- src/chapters/chapters.js
+++ src/chapters/chapters.js
@@ -46,12 +46,16 @@ export function createChapterCard(chapter) {
 }
 
 /**
- * Adds a story intro card and cards for each chapter to the UI.
+ * Fills the chapters bar with UI elements.
  *
  * @param {Story} story - The story to create the cards for
  */
 export function addChaptersBar(story) {
-  const cardsContainer = document.querySelector("#chapters-bar");
+  const barContainer = document.querySelector("#chapters-bar");
+
+  // Add card elements to the bar container
+  const cardsContainer = document.createElement("div");
+  cardsContainer.classList.add("cards");
 
   const storyIntroCard = createStoryIntroCard(story.properties);
   cardsContainer.appendChild(storyIntroCard);
@@ -60,4 +64,54 @@ export function addChaptersBar(story) {
     const chapterCard = createChapterCard(chapter);
     cardsContainer.appendChild(chapterCard);
   }
+
+  barContainer.appendChild(cardsContainer);
+
+  // Add navigation button elements to the bar container
+  const barNavigationButton = document.createElement("button");
+  barNavigationButton.classList.add("navigation-button");
+
+  const barNavigationPreviousButton = barNavigationButton.cloneNode(true);
+  barNavigationPreviousButton.classList.add("previous");
+  barNavigationPreviousButton.ariaLabel = "Show previous cards";
+
+  const barNavigationNextButton = barNavigationButton.cloneNode(true);
+  barNavigationNextButton.classList.add("next");
+  barNavigationNextButton.ariaLabel = "Show next cards";
+
+  barContainer.appendChild(barNavigationPreviousButton);
+  barContainer.appendChild(barNavigationNextButton);
+
+  // Add click event handlers to the navigation buttons
+  const cardElementWidth = cardsContainer.querySelector(".card").offsetWidth;
+  const cardsGap = Number(
+    getComputedStyle(cardsContainer).getPropertyValue("gap").replace("px", "")
+  );
+  const cardSectionWidth = cardElementWidth + cardsGap;
+
+  barNavigationPreviousButton.addEventListener("click", () => {
+    // Scroll one card back
+    const newScrollLeft = cardsContainer.scrollLeft - cardSectionWidth;
+    // Clamp new scroll position to the next full card on the left
+    const newClampedScrollLeft =
+      newScrollLeft - (newScrollLeft % cardSectionWidth);
+
+    cardsContainer.scrollTo({
+      left: newClampedScrollLeft,
+      behavior: "smooth",
+    });
+  });
+
+  barNavigationNextButton.addEventListener("click", () => {
+    // Scroll one card further
+    const newScrollLeft = cardsContainer.scrollLeft + cardSectionWidth;
+    // Clamp new scroll position to the next full card on the left
+    const newClampedScrollLeft =
+      newScrollLeft - (newScrollLeft % cardSectionWidth);
+
+    cardsContainer.scrollTo({
+      left: newClampedScrollLeft,
+      behavior: "smooth",
+    });
+  });
 }
