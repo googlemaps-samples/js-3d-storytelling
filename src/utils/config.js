3271e7de2547fab6d489294b31b157b782dbfdf0
immobe <i.beeck@gmail.com>
2023-12-14 16:25:07 +0100

    feat(configjs): remove deepEqual function

--- src/utils/config.js
+++ src/utils/config.js
@@ -21,30 +21,6 @@ const chapterProperties = [
 ];
 
 /**
- * Function to deep freeze an object.
- *
- * (See MDN deep freeze example: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/freeze)
- *
- * @param {Object} object - The object to freeze.
- * @returns {*} The frozen object.
- */
-function deepFreeze(object) {
-  // Retrieve the property names defined on object
-  const propNames = Reflect.ownKeys(object);
-
-  // Freeze properties before freezing self
-  for (const name of propNames) {
-    const value = object[name];
-
-    if ((value && typeof value === "object") || typeof value === "function") {
-      deepFreeze(value);
-    }
-  }
-
-  return Object.freeze(object);
-}
-
-/**
  * Asynchronously fetches and loads a configuration file in JSON format.
  *
  * If successful, the configuration data is returned for use in the application.
