7379cef7c0431d5ed096ce6f92f12afa749cc924
Frank Mecklenburg <mecklenburg@ubilabs.net>
2023-12-18 17:11:14 +0100

    feat(chapters-bar): add inline comments

--- src/chapters/chapters.js
+++ src/chapters/chapters.js
@@ -102,6 +102,8 @@ export function addChaptersBar(story) {
   );
   const cardSectionWidth = cardElementWidth + cardsGap;
 
+  // Check if the length and the width of the cards exceeds the container width.
+  // If yes sho naviagtion buttons
   if (
     cardSectionWidth * cardsContainer.children.length >
     cardsContainer.clientWidth
